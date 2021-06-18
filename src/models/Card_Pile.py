8491ddd69b760f4351543b703802d16287f0548b
Tim Brunson <tjbrunson@gmail.com>
2021-06-17 19:07:36 -0700

    added code to manage the card pile and determine a winner

--- src/models/Card_Pile.py
+++ src/models/Card_Pile.py
@@ -1,10 +1,12 @@
+from Deck import Deck
 class Card_Pile:
+
     def __init__(self):
         super().__init__()
         self.cards = []
         self.players = []
         # if there is a high card tie, add tie breaker collection here for payout
-        self.tie_pot = [] 
+        self.tie_pile = [] 
     
     '''
     want to append card and player to lists in order of card recieved
@@ -24,5 +26,21 @@ class Card_Pile:
     When initial dealing is done, any remainder cards are added here to ensure all players
     start on an even playing field
     '''
-    def add_to_tie_pot(self, cards):
-        self.tie_pot.extend(cards)
+    def add_to_tie_pile(self, cards):
+        self.tie_pile.extend(cards)
+        
+    def show_pile(self):
+        for player, card in zip(self.players, self.cards):
+            print("{} played a {}".format(player.name,card))
+            
+    def winner(self):
+        # Get list of value of each card (not suit) and then convert to list of ranks based on index
+        vals = [Deck.CARD_RANKS.index(val) for val in [card[1]  for card in self.cards]]
+        if vals.count(max(vals)) == 1:
+            return self.players[vals.index(max(vals))]
+    
+    def assign_winnings(self, winner):
+        winnings = self.cards + self.tie_pile
+        winner.add_all_cards(winnings)
+        self.cards = []
+        self.tie_pile = []
