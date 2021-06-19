f50ef719806648e06191222081b70cfe7abc1d59
Tim Brunson <tjbrunson@gmail.com>
2021-06-18 21:52:16 -0700

    converted suits to enum

--- src/models/Deck.py
+++ src/models/Deck.py
@@ -1,13 +1,24 @@
 import random
+from enum import Enum
+class Suits(Enum):
+    Spades = 'Spades'
+    Hearts = 'Hearts'
+    Diamonds = 'Diamonds'
+    Clubs = 'Clubs'
 
 class Deck:
-    SUITS = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
     CARD_RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace') # cards start at 2
     
     #Create list of cards based on suits and card ranks
     def __init__(self):
-        self.cards = [[s,r] for s in Deck.SUITS for r in Deck.CARD_RANKS]
+        self.cards = []
+        for suit in Suits:
+            for rank in Deck.CARD_RANKS:
+                self.cards.append([suit, rank])
         
     #shuffle deck using random.shuffle
     def shuffle(self):
         random.shuffle(self.cards)
+        
+deck = Deck()
+print(deck.cards)
