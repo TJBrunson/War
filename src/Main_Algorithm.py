bd8c4b5147ccbd7f56a9c5447f4f0224476f8ac3
Tim Brunson <tjbrunson@gmail.com>
2021-06-19 16:17:36 -0700

    added print statements to separate rounds

--- src/Main_Algorithm.py
+++ src/Main_Algorithm.py
@@ -6,6 +6,8 @@ game = Game(["Tim", "Emily", "Ed"])
 winner = None
 
 while(winner == None):
+    print()
     winner = game.play_round()
+    print()
     
 print("Congratulations {}, you won the game!".format(winner.name))
