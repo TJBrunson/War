7692eda7fee91dd4758fa42d622906cf23682680
Tim Brunson <tjbrunson@gmail.com>
2021-06-18 22:02:58 -0700

    removed keyword argument for hand because it should always be passed in and removed boolean check that was implicit

--- src/models/Player.py
+++ src/models/Player.py
@@ -2,13 +2,13 @@ from models.card_pile import Card_Pile
 class Player:
     
     #Currently create with list as hand. Empty list is default
-    def __init__(self, name, hand=[]):
+    def __init__(self, name, hand):
         self.name = name
         self.hand = hand
     
     #Returns if hand has any cards in it
     def has_cards(self):
-        return bool(len(self.hand) > 0)
+        return len(self.hand) > 0
     
     def play_card(self, card_pile):
          card_pile.add_card(self.hand.pop(0), self) # want to pop top card off stack
