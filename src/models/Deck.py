0e159b4ebe65d96eccd9976cdab842d8ddb6d452
Tim Brunson <tjbrunson@gmail.com>
2021-06-18 21:42:52 -0700

    using full suit and card names

--- src/models/Deck.py
+++ src/models/Deck.py
@@ -1,8 +1,8 @@
 import random
 
 class Deck:
-    SUITS = ('H', 'S', 'D', 'C')
-    CARD_RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A') # cards start at 2
+    SUITS = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
+    CARD_RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace') # cards start at 2
     
     #Create list of cards based on suits and card ranks
     def __init__(self):
