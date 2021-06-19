ab756f3358f7c90df45a8dde63b5ba86dc97a85b
Tim Brunson <tjbrunson@gmail.com>
2021-06-19 15:44:48 -0700

    added code to ask for number of players

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("hello")
+def get_num_of_players():
+    counter = 0
+    while counter < 3:
+        text = input()
+        try:
+            string_int = int(text)
+            return string_int
+        except ValueError:
+            counter+=1
+            print("Try entering a number less than 5")
+    return None
+
+print("Welcome to the card game War!")
+print("How many players are there? (max 4)")
+num = get_num_of_players()
+
+
